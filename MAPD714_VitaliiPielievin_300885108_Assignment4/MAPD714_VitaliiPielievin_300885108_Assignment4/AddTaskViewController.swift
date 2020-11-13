f66a237f033b7087b58bfd9d6b52f19940365fb9
VitaliiPi <44788449+VitaliiPi@users.noreply.github.com>
2020-11-13 20:59:58 +0200

    first commit

---
+++ MAPD714_VitaliiPielievin_300885108_Assignment4/MAPD714_VitaliiPielievin_300885108_Assignment4/AddTaskViewController.swift
@@ -0,0 +1,41 @@
+//
+//  AddTaskViewController.swift
+//  MAPD714_VitaliiPielievin_300885108_Assignment4
+//
+//  Created by vitalii on 2020-11-13.
+//  Copyright © 2020 vitalii. All rights reserved.
+//
+//  To Do App Assignment 4 - To Do List App UI
+//  Version: 1.0 - Basic UI
+//
+//  Student Name: Vitalii Pielievin
+//  Student ID:   300885108
+//  Date Started: 2020/11/13
+//
+//  To Do App allows us to add tasks, select time and add notes, by pressing Save button that task would be visible in TableView,
+//  if the task is completed, we can press check button and it will be greyed out, there are also buttons for update(edit) and remove tasks.
+//
+
+
+import UIKit
+
+class AddTaskViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
